09471e0e44ccacd3368677823d1af92165ba4051
gold-mir <miranda.keith.42@gmail.com>
2019-03-02 15:41:26 -0800

    add structure diagram and routes (without components yet)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Readme
+
+![](structure.png)
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,22 +3,29 @@ import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 
 //import components
-import Test from './test'
 
-let stuff = ["Cat", "Dog", "Bunny"]
+class App extends React.Component{
 
-function App(){
-  return (
-    <div>
-      <h1>Template</h1>
-      <Switch>
-        <Route exact path="/" render={(props) => {
-          return (<h3>Hello World!</h3>)
-        }} />
-        <Route exact path="/test" component={Test}/>
-      </Switch>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "Miranda Keith",
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <Switch>
+          <Route exact path='/' render={(props) => <div>This will be the home page</div>}/>
+          <Route exact path='/projects' render={(props) => <div>This will be a list of projects</div>}/>
+          <Route exact path='/admin' render={(props) => <div>This will be an admin page to add and remove projects</div>}/>
+          <Route render={(props) => <div>Page not found.</div>}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
